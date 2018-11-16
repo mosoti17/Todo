5f20fbfedbdb74b8714bac66dad861a9ff00600f
mosoti17 <mosoti17@gmail.com>
2018-11-16 16:08:53 +0300

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Elvis Mogaka on 11/16/18.
+//  Copyright © 2018 Elvis Mogaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
